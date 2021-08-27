7f6e2845b801ace9dfd83401a4bc6ae1ee9c76d4
Hieu Nguyen <73616304+hieunguyen0297@users.noreply.github.com>
2021-08-27 11:08:30 -0400

    first file in C#

---
+++ hello_world/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "hieu";
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("my name is " + name);
+            
+        }
+    }
+}

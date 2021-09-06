9d539ac34e8734949aed14964dc8f66815f35c01
Hieu Nguyen <73616304+hieunguyen0297@users.noreply.github.com>
2021-09-06 13:48:48 -0400

    create log file in C#

---
+++ logg/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+namespace logg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("logfile.txt")));
+
+            Trace.AutoFlush = true;
+
+            
+
+            Debug.WriteLine("Debug is watching");
+            Trace.WriteLine("hello, Trace is watching");
+
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json",
+            optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var ts = new TraceSwitch(
+                displayName: "PacktSwitch",
+                    description: "This switch is set via a JSON config.");
+
+            configuration.GetSection("PacktSwitch").Bind(ts);
+
+            Trace.WriteLineIf(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");   
+
+            //call AreaOfSquare() here
+            AreaOfSquare(10);
+        }
+        static void AreaOfSquare(int number){
+            Console.WriteLine($"Result is {number * number}");
+        }
+    }
+}
